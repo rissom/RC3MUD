104bbb0356ebcff9efce8da6a2b93974b0ed336c
Ferdinand Streicher <streicher@tentable.de>
2020-09-26 18:43:01 +0200

    actions array in player. action_say

--- python/game/player.py
+++ python/game/player.py
@@ -1,8 +1,15 @@
 import json
+import string
 from game.room import Room
 from system.helper import i18n
 
 class Player(object):
+    
+    actions = [ { "command": { "de": "sage"  , "en": "say" } ,
+                 "function" : "action_say",
+                "description":  { "de": "sagt", "en": "says" }
+                }
+              ]
     
     def __init__(self, wsclient):
         self.wsclient = wsclient
@@ -11,7 +18,15 @@ class Player(object):
         self.lang = "en"
         
         self.enter_room(1)
-        
+    
+    def action_say(self,a,msg):
+        for p in self.room.player:
+            ans = {
+              "cmd": "text",
+              "data": "\r\n"+self.name+" "+i18n(p.lang,a['description'])+" '"+msg[ len(i18n(self.lang,a['command']))+1:]+"'"
+            }
+            p.wsclient.write_message(ans)
+            
     def enter_room(self, roomid):
         newroom = Room.get_room_by_id(roomid)
         self.room.player_leaves_room(self)
@@ -36,26 +51,38 @@ class Player(object):
         self.room = newroom
         
     def parse_user_command(self, msg):
+        answered = False
         
+        for a in Player.actions:
+            if msg.startswith(i18n(self.lang,a['command'])):
+                getattr(self,a['function'])(a,msg)
+                answered = True
+
         if msg.startswith("whoami"):
             ans = {
               "cmd": "text",
               "data": "You are "+self.name
             }
             self.wsclient.write_message(json.dumps(ans))
+            answered = True
         elif msg.startswith("sleep"):
             ans = {
               "cmd": "text",
               "data": "You feel refreshed"
             }
             self.wsclient.write_message(json.dumps(ans))
+            answered = True
         elif msg.startswith("room2json"):
             ans = {
               "cmd": "text",
               "data": self.room.toJSON()
             }
             self.wsclient.write_message(json.dumps(ans))
-        elif not self.room.parse_user_command(self, msg):
+            answered = True
+        
+        if not answered:
+            answered = self.room.parse_user_command(self, msg)
+        if not answered:
             ans = {
               "cmd": "text",
               "data": "What you mean by '"+msg+"'"
